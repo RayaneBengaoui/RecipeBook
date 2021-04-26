d902e5ee8cced57e49f0ce30337264d97b298ab0
RayaneBengaoui <rayane.bengaoui@gmail.com>
2021-04-26 08:41:56 +0200

    Defining the structure of the App

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2018/03/28/20/32/food-3270461_960_720.jpg'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2018/03/28/20/32/food-3270461_960_720.jpg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
